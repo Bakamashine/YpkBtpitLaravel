4f9ddc75c7dc0f26b0609193ac048b677f0b8647
Bakamashine <ivan.fa.002@gmail.com>
2026-05-25 16:51:23 +0300

    push 2

---
+++ app/Http/Controllers/FavouriteController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Favourite;
+use App\Http\Requests\StoreFavouriteRequest;
+use App\Http\Requests\UpdateFavouriteRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class FavouriteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $favourites = $request->user()->favourite()->with('product')->paginate(6);
+        return view('user.favorite', compact('favourites'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFavouriteRequest $request)
+    {
+        $exists = $request->user()->favourite()
+            ->where('product_id', $request->validated('product_id'))
+            ->exists();
+
+        if (!$exists) {
+            $request->user()
+                ->favourite()
+                ->create($request->validated());
+            return back();
+        }
+
+        return back(422)
+            ->with("message", "Вы уже добавляли это в избранное");
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Favourite $favourite)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Favourite $favourite)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFavouriteRequest $request, Favourite $favourite)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        Favourite::where("product_id", $product->id)->delete();
+        return back();
+    }
+}
---
+++ app/Models/Favourite.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Favourite extends Model
+{
+    /** @use HasFactory<\Database\Factories\FavouriteFactory> */
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'product_id',
+    ];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function product(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+}
