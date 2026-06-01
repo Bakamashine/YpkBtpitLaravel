9c5ec174e57b8bef89adf91119db0757433d5d34
Bakamashine <ivan.fa.002@gmail.com>
2026-06-01 12:47:31 +0300

    Added the middleware for banned user

---
+++ app/Http/Middleware/BannedUserMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class BannedUserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && !auth()->user()->is_active) {
+            Auth::logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+            return redirect()->route("login")->with("banned", "Your account is blocked");
+        }
+        return $next($request);
+    }
+}
